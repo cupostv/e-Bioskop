25f05cbeb18d47bc197efdbac84b32622266aa08
cupostv <cupostv@gmail.com>
2016-01-16 12:07:45 +0100

    Update dodaj korisnika forma

---
+++ src/e_Bioskop/e_Bioskop/DistributerDodajForm.cs
@@ -0,0 +1,51 @@
+﻿using e_Bioskop.data.dao;
+using e_Bioskop.data.dao.mysql;
+using e_Bioskop.data.dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace e_Bioskop
+{
+    public partial class DistributerDodajForm : Form
+    {
+        public DistributerDodajForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            insertDistributer();
+        }
+
+        private DistributerDTO controlsToDistributer()
+        {
+            DistributerDTO distributer = new DistributerDTO();
+            distributer.Naziv = tbxNaziv.Text;
+            distributer.Adresa = tbxAdresa.Text;
+            distributer.Telefon = tbxBrojTelefona.Text;
+            distributer.Email = tbxEmail.Text;
+
+            return distributer;
+        }
+
+        private bool validate()
+        {
+            return true;
+        }
+
+        private void insertDistributer()
+        {
+            if (validate())
+            {
+                BioskopUtil.getDAOFactory().getDistributerDAO().insert(controlsToDistributer());
+            }
+        }
+    }
+}
--- src/e_Bioskop/e_Bioskop/Program.cs
+++ src/e_Bioskop/e_Bioskop/Program.cs
@@ -15,7 +15,7 @@ namespace e_Bioskop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DistributerDodajForm());
         }
     }
 }
