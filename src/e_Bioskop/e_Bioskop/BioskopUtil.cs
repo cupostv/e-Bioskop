47a40154436c8de436dcc10c3b21f6fa974e1282
RockLegend93 <milan.maric.ja@gmail.com>
2016-01-16 08:41:39 +0100

    Update util

---
+++ src/e_Bioskop/e_Bioskop/BioskopUtil.cs
@@ -0,0 +1,22 @@
+﻿using e_Bioskop.data.dao;
+using e_Bioskop.data.dao.mysql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace e_Bioskop
+{
+    public class BioskopUtil
+    {
+        private static DAOFactory daoFactory=null;
+        public static DAOFactory getDAOFactory()
+        {
+            if (daoFactory == null)
+            {
+                daoFactory = new MySqlDAOFactory();
+            }
+            return daoFactory;
+        }
+    }
+}
