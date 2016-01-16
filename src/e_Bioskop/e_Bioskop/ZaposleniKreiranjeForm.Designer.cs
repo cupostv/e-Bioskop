5bef81f5b83933fdfadc04f05488457043f8d73a
RockLegend93 <milan.maric.ja@gmail.com>
2016-01-16 14:25:39 +0100

    Update ZaposleniKreiranjeForm.cs

---
+++ src/e_Bioskop/e_Bioskop/ZaposleniKreiranjeForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace e_Bioskop
+{
+    partial class ZaposleniKreiranjeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbxLozinka = new System.Windows.Forms.TextBox();
+            this.lblKorisnickoIme = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxLozinka1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dtpDatumRodjenja = new System.Windows.Forms.DateTimePicker();
+            this.txbIme = new System.Windows.Forms.TextBox();
+            this.txbPrezime = new System.Windows.Forms.TextBox();
+            this.txbBrojTelefona = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.btnKreiraj = new System.Windows.Forms.Button();
+            this.cbAktivan = new System.Windows.Forms.CheckBox();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbxLozinka1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblKorisnickoIme);
+            this.groupBox1.Controls.Add(this.tbxLozinka);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 234);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(372, 145);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Podaci o korisničkom nalogu";
+            // 
+            // tbxLozinka
+            // 
+            this.tbxLozinka.Location = new System.Drawing.Point(124, 52);
+            this.tbxLozinka.Name = "tbxLozinka";
+            this.tbxLozinka.PasswordChar = '*';
+            this.tbxLozinka.Size = new System.Drawing.Size(232, 20);
+            this.tbxLozinka.TabIndex = 1;
+            // 
+            // lblKorisnickoIme
+            // 
+            this.lblKorisnickoIme.AutoSize = true;
+            this.lblKorisnickoIme.Location = new System.Drawing.Point(6, 22);
+            this.lblKorisnickoIme.Name = "lblKorisnickoIme";
+            this.lblKorisnickoIme.Size = new System.Drawing.Size(81, 13);
+            this.lblKorisnickoIme.TabIndex = 2;
+            this.lblKorisnickoIme.Text = "Korisničko ime: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lozinka";
+            // 
+            // tbxLozinka1
+            // 
+            this.tbxLozinka1.Location = new System.Drawing.Point(124, 85);
+            this.tbxLozinka1.Name = "tbxLozinka1";
+            this.tbxLozinka1.Size = new System.Drawing.Size(232, 20);
+            this.tbxLozinka1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Potvrdite lozinku";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cbAktivan);
+            this.groupBox2.Controls.Add(this.txbEmail);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dtpDatumRodjenja);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txbBrojTelefona);
+            this.groupBox2.Controls.Add(this.txbPrezime);
+            this.groupBox2.Controls.Add(this.txbIme);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 204);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Podaci o korisniku";
+            // 
+            // dtpDatumRodjenja
+            // 
+            this.dtpDatumRodjenja.Location = new System.Drawing.Point(124, 73);
+            this.dtpDatumRodjenja.Name = "dtpDatumRodjenja";
+            this.dtpDatumRodjenja.Size = new System.Drawing.Size(232, 20);
+            this.dtpDatumRodjenja.TabIndex = 0;
+            // 
+            // txbIme
+            // 
+            this.txbIme.Location = new System.Drawing.Point(124, 19);
+            this.txbIme.Name = "txbIme";
+            this.txbIme.Size = new System.Drawing.Size(232, 20);
+            this.txbIme.TabIndex = 1;
+            // 
+            // txbPrezime
+            // 
+            this.txbPrezime.Location = new System.Drawing.Point(124, 46);
+            this.txbPrezime.Name = "txbPrezime";
+            this.txbPrezime.Size = new System.Drawing.Size(232, 20);
+            this.txbPrezime.TabIndex = 2;
+            // 
+            // txbBrojTelefona
+            // 
+            this.txbBrojTelefona.Location = new System.Drawing.Point(124, 100);
+            this.txbBrojTelefona.Name = "txbBrojTelefona";
+            this.txbBrojTelefona.Size = new System.Drawing.Size(232, 20);
+            this.txbBrojTelefona.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ime:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Prezime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Datum rođenja:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 103);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Broj telefona:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 130);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "E-mail:";
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Location = new System.Drawing.Point(124, 127);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(232, 20);
+            this.txbEmail.TabIndex = 10;
+            // 
+            // btnKreiraj
+            // 
+            this.btnKreiraj.Location = new System.Drawing.Point(230, 396);
+            this.btnKreiraj.Name = "btnKreiraj";
+            this.btnKreiraj.Size = new System.Drawing.Size(75, 23);
+            this.btnKreiraj.TabIndex = 3;
+            this.btnKreiraj.Text = "Sačuvaj";
+            this.btnKreiraj.UseVisualStyleBackColor = true;
+            // 
+            // cbAktivan
+            // 
+            this.cbAktivan.AutoSize = true;
+            this.cbAktivan.Location = new System.Drawing.Point(124, 157);
+            this.cbAktivan.Name = "cbAktivan";
+            this.cbAktivan.Size = new System.Drawing.Size(62, 17);
+            this.cbAktivan.TabIndex = 11;
+            this.cbAktivan.Text = "Aktivan";
+            this.cbAktivan.UseVisualStyleBackColor = true;
+            // 
+            // btnOdustani
+            // 
+            this.btnOdustani.Location = new System.Drawing.Point(131, 396);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(75, 23);
+            this.btnOdustani.TabIndex = 4;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            // 
+            // ZaposleniKreiranjeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 419);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnKreiraj);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ZaposleniKreiranjeForm";
+            this.Text = "Kreiranje naloga";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbxLozinka;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxLozinka1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblKorisnickoIme;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txbBrojTelefona;
+        private System.Windows.Forms.TextBox txbPrezime;
+        private System.Windows.Forms.TextBox txbIme;
+        private System.Windows.Forms.DateTimePicker dtpDatumRodjenja;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.CheckBox cbAktivan;
+        private System.Windows.Forms.Button btnKreiraj;
+        private System.Windows.Forms.Button btnOdustani;
+    }
+}
