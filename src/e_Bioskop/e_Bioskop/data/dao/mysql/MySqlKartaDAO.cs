7503e78b0e66a0c576db801770f4eb7eec540a22
smihajlica <Sasa.Mihajlica@rt-rk.com>
2016-01-21 15:58:14 +0100

    getByProjekcija started

--- src/e_Bioskop/e_Bioskop/data/dao/mysql/MySqlKartaDAO.cs
+++ src/e_Bioskop/e_Bioskop/data/dao/mysql/MySqlKartaDAO.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using e_Bioskop.data.dto;
 
 namespace e_Bioskop.data.dao.mysql
 {
     public class MySqlKartaDAO : KartaDAO
     {
+    //    private string getByProjekcijaQuerry = "select K.idKarte, , K.datumProdaje, Z.idZaposleni, Z.korisnickoIme, Z.lozinka, Z.ime, Z.prezime, Z.
 
+    //    public List<KartaDTO> getByProjekcija(ProjekcijaDTO projekcija)
+    //    {
+    //        MySqlConnection connection = ConnectionPool.checkOutConnection();
+    //        MySqlCommand command = connection.CreateCommand();
+    //        command.CommandText = getByProjekcijaQuerry;
+    //        MySqlDataReader reader = command.ExecuteReader();
+    //    }
     }
 }
