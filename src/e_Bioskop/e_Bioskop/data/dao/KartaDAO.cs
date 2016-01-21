11127c79747f287c9645da5998bfa703585c9406
smihajlica <Sasa.Mihajlica@rt-rk.com>
2016-01-21 15:08:31 +0100

    Methods for KartaDTO

--- src/e_Bioskop/e_Bioskop/data/dao/KartaDAO.cs
+++ src/e_Bioskop/e_Bioskop/data/dao/KartaDAO.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using e_Bioskop.data.dto;
 
 namespace e_Bioskop.data.dao
 {
     public interface KartaDAO
     {
+        List<KartaDTO> getByProjekcija(ProjekcijaDTO projekcija);
+
+        List<KartaDTO> getByZaposleni(ZaposleniDTO zaposleni);
+
+        List<KartaDTO> getByStatus(StatusKartaDTO status);
+
+        KartaDTO getById(int id);
+
+        long insert(KartaDTO karta);
+
     }
 }
